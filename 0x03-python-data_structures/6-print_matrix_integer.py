774f212f3c29242c962460f8ece9064b5cd1ceb1
khalid10001 <khalid.2002.2011@gmail.com>
2023-12-04 13:27:54 -0800

    update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for sub in matrix:
+        if len(sub) == 0:
+            print()
+        for x in range(len(sub)):
+            print("{:d}".format(sub[n]), end="\n" if x is len(sub) - 1 else " "
