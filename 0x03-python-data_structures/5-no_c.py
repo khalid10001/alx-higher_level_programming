1733da1153b68d4987af47ade9b34f90c915bff9
khalid10001 <khalid.2002.2011@gmail.com>
2023-12-04 13:24:37 -0800

    update

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    ret = ""
+    for x in range(len(my_string)):
+        if (my_string[n] != 'c' and my_string[n] != 'C'):
+            ret += my_string[x]
+    return ret
