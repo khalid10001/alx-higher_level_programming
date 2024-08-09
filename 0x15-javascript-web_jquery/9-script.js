51bd6b8410f6cbb0395b0b136e908713dd3ffb85
khalid10001 <khalid.2002.2011@gmail.com>
2024-08-09 06:40:49 -0700

    update

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,5 @@
+9-script.js9-script.js9-script.js$('document').ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
+});
